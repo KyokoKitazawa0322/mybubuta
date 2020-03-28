512c03a2a9bb050031cb366f66e7c2d93ed475e0
kyokokitazawa <59463972+KyokoKitazawa0322@users.noreply.github.com>
2020-03-28 17:30:53 +0900

    update

--- left_pane.php
+++ left_pane.php
@@ -1,8 +1,7 @@
-
+<!--
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/bxslider/4.2.12/jquery.bxslider.css">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 <script src="https://cdn.jsdelivr.net/bxslider/4.2.12/jquery.bxslider.min.js"></script>
-		  
 <script>
 $(document).ready(function(){
   $('.bunner').bxSlider({
@@ -15,7 +14,7 @@ $(document).ready(function(){
   slideWidth: 200,
                     });
 });
-</script>
+</script>-->
 
 <div id="leftbox">
     <div class="bunner">
