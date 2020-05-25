4e6a2cae7e891bb51f1fddd108e5efced7283d8b
kyokokitazawa <59463972+KyokoKitazawa0322@users.noreply.github.com>
2020-05-25 16:16:01 +0900

    POST値取得方法、例外処理対応範囲修正等

--- app/controllers/MyPageUpdateAction.php
+++ app/controllers/MyPageUpdateAction.php
@@ -43,109 +43,109 @@ class MyPageUpdateAction {
             $_SESSION['from_order_flag']= $_POST['cmd'];   
         }
 
-    /**--------------------------------------------------------
-      会員情報の取得
-     ---------------------------------------------------------*/
+        $customerDao = new CustomerDao();
+        $validator = new CommonValidator();
         
         try{
-            $customerDao = new CustomerDao();
-            $validator = new CommonValidator();
-            
-            $customerDto = $customerDao->getCustomerById($customerId);
-            $this->customerDto = $customerDto;
+            $this->customerDto = $customerDao->getCustomerById($customerId);
+        }catch(\PDOException $e){
+            die('SQLエラー :'.$e->getMessage());
+        }
     /**--------------------------------------------------------
      * sessionに格納しバリデーションチェック
      ---------------------------------------------------------*/
-            if(isset($_POST['cmd']) && $_POST['cmd']=="confirm"){
-                $lastName = $_POST['last_name'];
-                $firstName = $_POST['first_name'];
-                $rubyLastName = $_POST['ruby_last_name'];
-                $rubyFirstName = $_POST['ruby_first_name'];
-                $address01 = $_POST['address01'];
-                $address02 = $_POST['address02'];
-                $address03 = $_POST['address03'];
-                $address04 = $_POST['address04'];
-                $address05 = $_POST['address05'];
-                $address06 = $_POST['address06'];
-                $tel = $_POST['tel'];
-                $mail = $_POST['mail'];
+        if(isset($_POST['cmd']) && $_POST['cmd']=="confirm"){
+            $lastName = filter_input(INPUT_POST, 'last_name');
+            $firstName = filter_input(INPUT_POST, 'first_name');
+            $rubyLastName = filter_input(INPUT_POST, 'ruby_last_name');
+            $rubyFirstName = filter_input(INPUT_POST, 'ruby_first_name');
+            $address01 = filter_input(INPUT_POST, 'address01');
+            $address02 = filter_input(INPUT_POST, 'address02');
+            $address03 = filter_input(INPUT_POST, 'address03');
+            $address04 = filter_input(INPUT_POST, 'address04');
+            $address05 = filter_input(INPUT_POST, 'address05');
+            $address06 = filter_input(INPUT_POST, 'address06');
+            $tel = filter_input(INPUT_POST, 'tel');
+            $mail = filter_input(INPUT_POST, 'mail');
 
-                if(empty($_POST['password']) && empty($_POST['password_confirm'])){
-                    $password = $_COOKIE['password'];
-                    $passwordConfirm = $_COOKIE['password'];
-                }else{
-                    $password = $_POST['password'];   
-                    $passwordConfirm = $_POST['password_confirm'];
-                    $_SESSION['password_input'] = TRUE;
-                }
+            if(empty($_POST['password']) && empty($_POST['password_confirm'])){
+                $password = $_COOKIE['password'];
+                $passwordConfirm = $_COOKIE['password'];
+            }else{
+                $password = $_POST['password'];   
+                $passwordConfirm = $_POST['password_confirm'];
+                $_SESSION['password_input'] = TRUE;
+            }
 
-                $_SESSION['update'] = array(
-                 'last_name' => $lastName,
-                 'first_name' => $firstName,
-                 'ruby_last_name' => $rubyLastName,
-                 'ruby_first_name' => $rubyFirstName,
-                 'address01' => $address01,
-                 'address02' => $address02,
-                 'address03' => $address03,
-                 'address04' => $address04,
-                 'address05' => $address05,
-                 'address06' => $address06,
-                 'tel' => $tel,
-                 'mail' => $mail,
-                 'password' => $password,
-                 'password_confirm' => $passwordConfirm
-                );
+            $_SESSION['update'] = array(
+             'last_name' => $lastName,
+             'first_name' => $firstName,
+             'ruby_last_name' => $rubyLastName,
+             'ruby_first_name' => $rubyFirstName,
+             'address01' => $address01,
+             'address02' => $address02,
+             'address03' => $address03,
+             'address04' => $address04,
+             'address05' => $address05,
+             'address06' => $address06,
+             'tel' => $tel,
+             'mail' => $mail,
+             'password' => $password,
+             'password_confirm' => $passwordConfirm
+            );
 
-                $key = "氏名(性)";
-                $this->lastNameError = $validator->fullWidthValidation($key, $lastName);
+            $key = "氏名(性)";
+            $this->lastNameError = $validator->fullWidthValidation($key, $lastName);
 
-                $key = "氏名(名)";
-                $this->firstNameError = $validator->fullWidthValidation($key, $firstName);
+            $key = "氏名(名)";
+            $this->firstNameError = $validator->fullWidthValidation($key, $firstName);
 
-                $key = "氏名(セイ)";
-                $this->rubyLastNameError = $validator->rubyValidation($key, $rubyLastName);
+            $key = "氏名(セイ)";
+            $this->rubyLastNameError = $validator->rubyValidation($key, $rubyLastName);
 
-                $key = "氏名(メイ)";
-                $this->rubyFirstNameError = $validator->rubyValidation($key, $rubyFirstName);
+            $key = "氏名(メイ)";
+            $this->rubyFirstNameError = $validator->rubyValidation($key, $rubyFirstName);
 
-                $key = "郵便番号(3ケタ)";
-                $this->address01Error = $validator->firstZipCodeValidation($key, $address01);
+            $key = "郵便番号(3ケタ)";
+            $this->address01Error = $validator->firstZipCodeValidation($key, $address01);
 
-                $key = "郵便番号(4ケタ)";
-                $this->address02Error  = $validator->lastZipCodeValidation($key, $address02);
+            $key = "郵便番号(4ケタ)";
+            $this->address02Error  = $validator->lastZipCodeValidation($key, $address02);
 
-                $key="都道府県";
-                $this->address03Error = $validator->requireCheck($key, $address03);
+            $key="都道府県";
+            $this->address03Error = $validator->requireCheck($key, $address03);
 
-                $key="市区町村";
-                $this->address04Error = $validator->requireCheck($key, $address04);
+            $key="市区町村";
+            $this->address04Error = $validator->requireCheck($key, $address04);
 
-                $key="番地";
-                $this->address05Error = $validator->requireCheck($key, $address05);
+            $key="番地";
+            $this->address05Error = $validator->requireCheck($key, $address05);
 
-                $key="メールアドレス";
-                $this->mailError = $validator->mailValidation($key, $mail);
-                if(!$this->mailError){
+            $key="メールアドレス";
+            $this->mailError = $validator->mailValidation($key, $mail);
+            
+            if(!$this->mailError){
+                try{
                     $this->mailError = $validator->checkMail($mail, $this->customerDto->getMail());
-                }
-                
-                $key="電話番号";
-                $this->telError = $validator->telValidation($key, $tel);
-
-                $key="パスワード";
-                $this->passwordError = $validator->passValidation($key, $password);
-
-                $key="パスワード(再確認)";
-                $this->passwordConfirmError = $validator->passConfirmValidation($key, $passwordConfirm, $password);
-
-                if($validator->getResult()) {
-                    $_SESSION['update']['input'] = TRUE;
-                    header('Location:/html/mypage/mypage_update_confirm.php');
-                    exit();
+                }catch(\PDOException $e){
+                    die('SQLエラー :'.$e->getMessage());
                 }
             }
-        }catch(\PDOException $e){
-            die('SQLエラー :'.$e->getMessage());
+
+            $key="電話番号";
+            $this->telError = $validator->telValidation($key, $tel);
+
+            $key="パスワード";
+            $this->passwordError = $validator->passValidation($key, $password);
+
+            $key="パスワード(再確認)";
+            $this->passwordConfirmError = $validator->passConfirmValidation($key, $passwordConfirm, $password);
+
+            if($validator->getResult()) {
+                $_SESSION['update']['input'] = TRUE;
+                header('Location:/html/mypage/mypage_update_confirm.php');
+                exit();
+            }
         }
     }
     
