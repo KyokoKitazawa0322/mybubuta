3d230c3b2cb3d68ec58543a6b3e3c4df2a005a8d
kyokokitazawa <59463972+KyokoKitazawa0322@users.noreply.github.com>
2020-07-03 21:32:36 +0900

    ページ不正遷移対策処理追加、例外処理修正等

--- app/controllers/RegisterCompleteAction.php
+++ app/controllers/RegisterCompleteAction.php
@@ -2,7 +2,10 @@
 namespace Controllers;
 use \Models\CustomerDao;
 use \Models\CustomersDto;
-use \Models\OriginalException;
+use \Models\DBParamException;
+use \Models\NoRecordException;
+use \Models\InvalidParamException;
+use \Models\MyPDOException;
 use \Config\Config;
 
 class RegisterCompleteAction{
@@ -14,46 +17,57 @@ class RegisterCompleteAction{
         /*====================================================================
         　register_confirm.phpで「この内容で登録をする」ボタンが押された時の処理
         =====================================================================*/
-        if($cmd == "complete" && isset($_SESSION['register'])){
-            
-            $lastName = $_SESSION['register']['last_name'];
-            $firstName = $_SESSION['register']['first_name'];
-            $rubyLastName = $_SESSION['register']['ruby_last_name'];
-            $rubyFirstName = $_SESSION['register']['ruby_first_name'];
-            $zipCode01 = $_SESSION['register']['zip_code_01'];
-            $zipCode02 = $_SESSION['register']['zip_code_02'];
-            $prefecture = $_SESSION['register']['prefecture'];
-            $city = $_SESSION['register']['city'];
-            $blockNumber = $_SESSION['register']['block_number'];
-            $buildingName = $_SESSION['register']['building_name'];
-            $tel = $_SESSION['register']['tel'];
-            $mail = $_SESSION['register']['mail'];
-            $password = $_SESSION['register']['password'];
-            
+        
+        try{
+            $this->checkToken();
+                
+        }catch(InvalidParamException $e){
+            $e->handler($e);   
+        }
+
+        $lastName = $_SESSION['register']['last_name'];
+        $firstName = $_SESSION['register']['first_name'];
+        $rubyLastName = $_SESSION['register']['ruby_last_name'];
+        $rubyFirstName = $_SESSION['register']['ruby_first_name'];
+        $zipCode01 = $_SESSION['register']['zip_code_01'];
+        $zipCode02 = $_SESSION['register']['zip_code_02'];
+        $prefecture = $_SESSION['register']['prefecture'];
+        $city = $_SESSION['register']['city'];
+        $blockNumber = $_SESSION['register']['block_number'];
+        $buildingName = $_SESSION['register']['building_name'];
+        $tel = $_SESSION['register']['tel'];
+        $mail = $_SESSION['register']['mail'];
+        $password = $_SESSION['register']['password'];
+
+        try{
             $customerDao = new CustomerDao();
             
-            try{
-                $customerDao->insertCustomerInfo($password, $lastName, $firstName, $rubyLastName, $rubyFirstName, $zipCode01, $zipCode02, $prefecture, $city, $blockNumber, $buildingName, $tel, $mail);
-                
-                $customerDto = $customerDao->getCustomerByMail($mail); 
-            
-                $_SESSION['customer_id'] = $customerDto->getCustomerId();
-                unset($_SESSION['register']); 
-                
-            } catch(\PDOException $e){
-                Config::outputLog($e->getCode(), $e->getMessage(), $e->getTraceAsString());;
-                header('Content-Type: text/plain; charset=UTF-8', true, 500);
-                die('エラー:データベースの処理に失敗しました。');
+            $customerDao->insertCustomerInfo($password, $lastName, $firstName, $rubyLastName, $rubyFirstName, $zipCode01, $zipCode02, $prefecture, $city, $blockNumber, $buildingName, $tel, $mail);
 
-            }catch(OriginalException $e){
-                Config::outputLog($e->getCode(), $e->getMessage(), $e->getTraceAsString());
-                header('Content-Type: text/plain; charset=UTF-8', true, 400);
-                die('エラー:'.$e->getMessage());
-            }
+            $customerDto = $customerDao->getCustomerByMail($mail); 
+            $_SESSION['customer_id'] = $customerDto->getCustomerId();
             
+            unset($_SESSION['register']); 
+
+        } catch(MyPDOException $e){
+            $e->handler($e);
+        }
+    }
+    
+    /*---------------------------------------*/
+    //トークンをセッションから取得
+    public function checkToken(){
+        $tokenComplete = filter_input(INPUT_POST, "token_complete");
+        //セッションがないか生成したトークンと異なるトークンでPOSTされたときは不正アクセス
+        if(!isset($_SESSION['token']['complete']) || ($_SESSION['token']['complete'] != $tokenComplete)){
+            if(!isset($_SESSION['token']['complete'])){
+                $sessionTokenComplete = "nothing"; 
+            }else{
+                $sessionTokenComplete = $_SESSION['token']['complete'];   
+            }
+            throw new InvalidParamException('Invalid param for register_complete:$tokenComplete='.$tokenComplete.'/$_SESSION["token"]["complete"]='.$sessionTokenComplete);
         }else{
-            header('Location:/html/item_list.php');
-            exit();
+            unset($_SESSION['token']);   
         }
     }
 }
--- app/controllers/RegisterConfirmAction.php
+++ app/controllers/RegisterConfirmAction.php
@@ -1,14 +1,43 @@
 <?php
 namespace Controllers;
-use \Models\CustomerDao;
-use \Models\CustomersDto;
+
+use \Models\InvalidParamException;
 
 class RegisterConfirmAction{
         
     public function execute(){
         
-        if($_SESSION['register']['input'] != "complete"){
-            header('Location:/html/item_list.php');
+        try{
+            $this->checkValidationResult();
+            $this->setToken();
+            
+        }catch(InvalidParamException $e){
+            $e->handler($e);   
         }
+    }
+    
+    /*---------------------------------------*/
+    
+    /**
+    * バリデーションを通過してきたか確認
+    * throw InvalidParamException
+    **/
+    public function checkValidationResult(){
+        
+        if(!isset($_SESSION['register_data']) || $_SESSION['register_data'] != "complete"){
+            if(!isset($_SESSION['register_data'])){
+                $registerData = "nothing";
+            }else{
+                $registerData = $_SESSION['register_data'];   
+            }
+            throw new InvalidParamException('Invalid param for update_confirm:$_SESSION["register_data"]='.$registerData);
+        }
+    }
+    
+    /*---------------------------------------*/
+    //トークンをセッションにセット
+    public function setToken(){
+        $token = sha1(uniqid(mt_rand(), true));
+        $_SESSION['token']['complete'] = $token;
     }
 }
