509a8ef4e4029b742d74f24caedaea813db53508
kyokokitazawa <59463972+KyokoKitazawa0322@users.noreply.github.com>
2020-07-03 18:12:08 +0900

    ページ不正遷移対策処理追加、マイページ共通メソッド追加、例外処理修正等

--- app/controllers/MyPageDeliveryAddCompleteAction.php
+++ app/controllers/MyPageDeliveryAddCompleteAction.php
@@ -1,75 +1,89 @@
 <?php
 namespace Controllers;
+
 use \Models\DeliveryDao;
-use \Models\OriginalException;
+
 use \Config\Config;
 
-class MyPageDeliveryAddCompleteAction {
+use \Models\DBParamException;
+use \Models\NoRecordException;
+use \Models\InvalidParamException;
+use \Models\MyPDOException;
+
+class MyPageDeliveryAddCompleteAction extends \Controllers\CommonMyPageAction{
     
     public function execute(){
         
         $cmd = filter_input(INPUT_GET, 'cmd');
         
-        if($cmd == "do_logout" ){
-            unset($_SESSION['customer_id']);
-        }
-        
-        if(!isset($_SESSION["customer_id"])){
-            header("Location:/html/login.php");   
-            exit();
-        }else{
-            $customerId = $_SESSION['customer_id'];   
-        }
+        $this->checkLogoutRequest($cmd);
+        $this->checkLogin();
+        $customerId = $_SESSION['customer_id'];   
         
         /*====================================================================
-            「配送先保存ボタン」が押された時の処理
-        =====================================================================*/
+         mypage_delivery_add_confirm.phpで「この内容で登録をする」ボタンが押された時の処理
+         =====================================================================*/
         
-        if(isset($_SESSION['add_data']) && $_SESSION['add_data'] == "clear") { 
-            
-            $lastName = $_SESSION['del_add']['last_name'];
-            $firstName = $_SESSION['del_add']['first_name'];
-            $rubyLastName = $_SESSION['del_add']['ruby_last_name'];
-            $rubyFirstName = $_SESSION['del_add']['ruby_first_name'];
-            $zipCode01 = $_SESSION['del_add']['zip_code_01'];
-            $zipCode02 = $_SESSION['del_add']['zip_code_02'];
-            $prefecture = $_SESSION['del_add']['prefecture'];
-            $city = $_SESSION['del_add']['city'];
-            $blockNumber = $_SESSION['del_add']['block_number'];
-            $buildingName = $_SESSION['del_add']['building_name'];
-            $tel = $_SESSION['del_add']['tel'];
-            
-            try{
-                $deliveryDao = new DeliveryDao();
+        try{
+            $this->checkToken();
                 
-                $deliveryDao->insertDeliveryInfo($lastName, $firstName, $rubyLastName, $rubyFirstName, $zipCode01, $zipCode02, $prefecture, $city, $blockNumber, $buildingName, $tel, $customerId);
-                
-            } catch(\PDOException $e){
-                Config::outputLog($e->getCode(), $e->getMessage(), $e->getTraceAsString());;
-                header('Content-Type: text/plain; charset=UTF-8', true, 500);
-                die('エラー:データベースの処理に失敗しました。');
+        }catch(InvalidParamException $e){
+            $e->handler($e);   
+        }
 
-            }catch(OriginalException $e){
-                Config::outputLog($e->getCode(), $e->getMessage(), $e->getTraceAsString());
-                header('Content-Type: text/plain; charset=UTF-8', true, 400);
-                die('エラー:'.$e->getMessage());
-            }
+        $lastName = $_SESSION['del_add']['last_name'];
+        $firstName = $_SESSION['del_add']['first_name'];
+        $rubyLastName = $_SESSION['del_add']['ruby_last_name'];
+        $rubyFirstName = $_SESSION['del_add']['ruby_first_name'];
+        $zipCode01 = $_SESSION['del_add']['zip_code_01'];
+        $zipCode02 = $_SESSION['del_add']['zip_code_02'];
+        $prefecture = $_SESSION['del_add']['prefecture'];
+        $city = $_SESSION['del_add']['city'];
+        $blockNumber = $_SESSION['del_add']['block_number'];
+        $buildingName = $_SESSION['del_add']['building_name'];
+        $tel = $_SESSION['del_add']['tel'];
 
-            unset($_SESSION['add_data']);
-            unset($_SESSION['del_add']);
-            
-            /*——————————————————————————————————————————————————————————————
-                order_delivery_listからきた場合
-            ————————————————————————————————————————————————————————————————*/
-            
-            if(isset($_SESSION['from_order_flag'])){
-                unset($_SESSION['from_order_flag']);
-                header('Location:/html/order/order_delivery_list.php');
-                exit();
-            }
-        }else{
-            header("Location:/html/login.php");   
+        try{
+            $deliveryDao = new DeliveryDao();
+            $deliveryDao->insertDeliveryInfo($lastName, $firstName, $rubyLastName, $rubyFirstName, $zipCode01, $zipCode02, $prefecture, $city, $blockNumber, $buildingName, $tel, $customerId);
+
+        } catch(MyPDOException $e){
+            $e->handler($e);
+        }
+
+        unset($_SESSION['add_data']);
+        unset($_SESSION['del_add']);
+
+        /*——————————————————————————————————————————————————————————————
+            order_delivery_listからきた場合
+        ————————————————————————————————————————————————————————————————*/
+
+        if(isset($_SESSION['from_order_flag'])){
+            unset($_SESSION['from_order_flag']);
+            header('Location:/html/order/order_delivery_list.php');
             exit();
+        }
+    }
+    
+    /*---------------------------------------*/
+    //トークンをセッションから取得
+    public function checkToken(){
+        $tokenDelAddComplete = filter_input(INPUT_POST, "token_del_add_complete");
+        //セッションがないか生成したトークンと異なるトークンでPOSTされたときは不正アクセス
+        if(!isset($_SESSION['token']['del_add_complete']) || ($_SESSION['token']['del_add_complete'] != $tokenDelAddComplete || !isset($_SESSION['add_data']))){
+            if(!isset($_SESSION['token']['del_add_complete'])){
+                $sessionTokenDelAddComplete = "nothing"; 
+            }else{
+                $sessionTokenDelAddComplete = $_SESSION['token']['del_add_complete'];   
+            }
+            if(!isset($_SESSION['add_data'])){
+                $addData = "nothing";   
+            }else{
+                $addData = $_SESSION['add_data'];   
+            }
+            throw new InvalidParamException('Invalid param for register_complete:$tokenDelAddComplete='.$tokenDelAddComplete.'/$_SESSION["token"]["del_add_complete"]='.$sessionTokenDelAddComplete.'/$_SESSION["add_data"]='.$addData);
+        }else{
+            unset($_SESSION['token']);   
         }
     }
 }
--- app/controllers/MyPageDeliveryCompleteAction.php
+++ app/controllers/MyPageDeliveryCompleteAction.php
@@ -1,77 +1,102 @@
 <?php
 namespace Controllers;
+
 use \Models\DeliveryDao;
 use \Models\CustomerDao;    
-use \Models\OriginalException;
+
 use \Config\Config;
 
-class MyPageDeliveryCompleteAction {
+use \Models\DBParamException;
+use \Models\NoRecordException;
+use \Models\InvalidParamException;
+use \Models\MyPDOException;
+
+class MyPageDeliveryCompleteAction extends \Controllers\CommonMyPageAction{
     
     public function execute(){
         
         $cmd = filter_input(INPUT_POST, 'cmd');
         
-        if($cmd == "do_logout" ){
-            unset($_SESSION['customer_id']);
-        }
-        
-        if(!isset($_SESSION["customer_id"])){
-            header("Location:/html/login.php");   
-            exit();
-        }else{
-            $customerId = $_SESSION['customer_id'];   
-        }        
+        $this->checkLogoutRequest($cmd);
+        $this->checkLogin();
+        $customerId = $_SESSION['customer_id'];     
 
         /*====================================================================
-            /配送先保存ボタンが押された時の処理
-        =====================================================================*/
+         mypage_delivery_entry_confirm.phpで「この内容で登録をする」ボタンが押された時の処理
+         =====================================================================*/
+        
+        try{
+            $this->checkToken();
+                
+        }catch(InvalidParamException $e){
+            $e->handler($e);   
+        }
 
-        if(isset($_SESSION['update_data']) && $_SESSION['update_data'] == "clear"){ 
-            
-            $lastName = $_SESSION['del_update']['last_name'];
-            $firstName = $_SESSION['del_update']['first_name'];
-            $rubyLastName = $_SESSION['del_update']['ruby_last_name'];
-            $rubyFirstName = $_SESSION['del_update']['ruby_first_name'];
-            $zipCode01 = $_SESSION['del_update']['zip_code_01'];
-            $zipCode02 = $_SESSION['del_update']['zip_code_02'];
-            $prefecture = $_SESSION['del_update']['prefecture'];
-            $city = $_SESSION['del_update']['city'];
-            $blockNumber = $_SESSION['del_update']['block_number'];
-            $buildingName = $_SESSION['del_update']['building_name'];
-            $tel = $_SESSION['del_update']['tel'];
-            
-            $deliveryId = $_SESSION['del_id'];
-            $deliveryDao = new DeliveryDao();
-            
-            try{
-                $deliveryDao->updateDeliveryInfo($lastName, $firstName, $rubyLastName, $rubyFirstName, $zipCode01, $zipCode02, $prefecture, $city, $blockNumber, $buildingName, $tel, $customerId, $deliveryId);
-                
-                unset($_SESSION['update_data']);
-                unset($_SESSION['del_update']);
-                
-            } catch(\PDOException $e){
-                Config::outputLog($e->getCode(), $e->getMessage(), $e->getTraceAsString());;
-                header('Content-Type: text/plain; charset=UTF-8', true, 500);
-                die('エラー:データベースの処理に失敗しました。');
-                
-            }catch(OriginalException $e){
-                Config::outputLog($e->getCode(), $e->getMessage(), $e->getTraceAsString());
-                header('Content-Type: text/plain; charset=UTF-8', true, 400);
-                die('エラー:'.$e->getMessage());
-            }
-    
-            /*——————————————————————————————————————————————————————————————
-                order_delivery_listからきた場合
-            ————————————————————————————————————————————————————————————————*/
-            
-            if(isset($_SESSION['from_order_flag'])){
-                header('Location:/html/order/order_delivery_list.php');
-                unset($_SESSION['from_order_flag']);
-                exit();
-            }
-        }else{
-            header("Location:/html/login.php");   
+        $lastName = $_SESSION['del_update']['last_name'];
+        $firstName = $_SESSION['del_update']['first_name'];
+        $rubyLastName = $_SESSION['del_update']['ruby_last_name'];
+        $rubyFirstName = $_SESSION['del_update']['ruby_first_name'];
+        $zipCode01 = $_SESSION['del_update']['zip_code_01'];
+        $zipCode02 = $_SESSION['del_update']['zip_code_02'];
+        $prefecture = $_SESSION['del_update']['prefecture'];
+        $city = $_SESSION['del_update']['city'];
+        $blockNumber = $_SESSION['del_update']['block_number'];
+        $buildingName = $_SESSION['del_update']['building_name'];
+        $tel = $_SESSION['del_update']['tel'];
+
+        $deliveryId = $_SESSION['del_id'];
+        $deliveryDao = new DeliveryDao();
+
+        try{
+            $deliveryDao->updateDeliveryInfo($lastName, $firstName, $rubyLastName, $rubyFirstName, $zipCode01, $zipCode02, $prefecture, $city, $blockNumber, $buildingName, $tel, $customerId, $deliveryId);
+
+            unset($_SESSION['delivery_entry_data']);
+            unset($_SESSION['del_update']);
+            unset($_SESSION['del_id']);
+
+        } catch(MyPDOException $e){
+            $e->handler($e);
+
+        }catch(DBParamException $e){
+            $e->handler($e);
+        }
+
+        /*——————————————————————————————————————————————————————————————
+            order_delivery_listからきた場合
+        ————————————————————————————————————————————————————————————————*/
+
+        if(isset($_SESSION['from_order_flag'])){
+            unset($_SESSION['from_order_flag']);
+            header('Location:/html/order/order_delivery_list.php');
             exit();
+        }
+    }
+    
+    /*---------------------------------------*/
+    //トークンをセッションから取得
+    public function checkToken(){
+        
+        $tokenEntryUpdateComplete = filter_input(INPUT_POST, "token_entry_update_complete");
+        //セッションがないか生成したトークンと異なるトークンでPOSTされたときは不正アクセス
+        
+        if(!isset($_SESSION['token']['entry_update_complete']) || ($_SESSION['token']['entry_update_complete'] != $tokenEntryUpdateComplete || !isset($_SESSION['delivery_entry_data']))){
+            
+            if(!isset($_SESSION['token']['entry_update_complete'])){
+                $sessionTokenEntryUpdateComplete = "nothing"; 
+            }else{
+                $sessionTokenEntryUpdateComplete = $_SESSION['token']['entry_update_complete'];   
+            
+            }
+            if(!isset($_SESSION['delivery_entry_data'])){
+                $deliveryEntryData = "nothing";   
+            }else{
+                $deliveryEntryData = $_SESSION['delivery_entry_data'];   
+            }
+            
+            throw new InvalidParamException('Invalid param for register_complete:$tokenEntryUpdateComplete='.$tokenEntryUpdateComplete.'/$_SESSION["token"]["entry_update_complete"]='.$sessiontokenEntryUpdateComplete.'/$_SESSION["delivery_entry_data"]='.$deliveryEntryData);
+        
+        }else{
+            unset($_SESSION['token']);   
         }
     }
 }
