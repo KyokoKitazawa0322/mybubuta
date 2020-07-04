7c57b8e148029d6a6f386967c220d579ca2c23a8
kyokokitazawa <59463972+KyokoKitazawa0322@users.noreply.github.com>
2020-07-04 21:46:21 +0900

    都道府県値チェックメソッド追加、textarea用入力値検査追加等

--- app/models/CommonValidator.php
+++ app/models/CommonValidator.php
@@ -1,14 +1,15 @@
 <?php 
 namespace Models;
 use \Models\CustomerDao;
+use \Config\Config;
     
 class CommonValidator {
     
-    private $result =true;
+    private $result = TRUE;
 //---------------------------------------
     //ログイン用
     public function requiredError($password, $mail) {
-        $error = false;
+        $error = FALSE;
         $item = array(
             'パスワード' => $password,
             'メール' => $mail
@@ -22,10 +23,21 @@ class CommonValidator {
     }
     //---------------------------------------
     public function requireCheck($key, $value) {
-        $error = false;
+        $error = FALSE;
         if(empty($value)) {
             $error = $key.'は必須入力です。';
-            $this->result = false;
+            $this->result = FALSE;
+        }
+        return $error; 
+    }
+    
+    //---------------------------------------
+    public function requireCheckForTextarea($key, $value) {
+        $error = FALSE;
+        $value_trim = trim($value);
+        if($value_trim == "") {
+            $error = $key.'は必須入力です。';
+            $this->result = FALSE;
         }
         return $error; 
     }
@@ -44,19 +56,19 @@ class CommonValidator {
                 $error = "既に使用されているメールアドレスです。";
             }
         }
-        $this->result = false;
+        $this->result = FALSE;
         return $error; 
     }
     
     //---------------------------------------
     public function fullWidthValidation($key, $value) {
-        $error = false;
+        $error = FALSE;
         if(empty($value)) {
             $error = $key.'は必須入力です。';
-            $this->result = false;
+            $this->result = FALSE;
         }elseif(!preg_match('/^[ぁ-んァ-ヶー一-龠 　\r\n\t]+$/u',$value)){
             $error = $key.'は全角文字で入力して下さい。';
-            $this->result = false;
+            $this->result = FALSE;
         }
         return $error; 
     }
@@ -64,13 +76,13 @@ class CommonValidator {
     //---------------------------------------
 
     public function rubyValidation($key, $value) {
-        $error = false;
+        $error = FALSE;
         if(empty($value)) {
             $error = $key.'は必須入力です。';
-            $this->result = false;
+            $this->result = FALSE;
         }elseif(!preg_match('/^[ア-ン゛゜ァ-ォャ-ョー「」、]+$/u',$value)) {
             $error = $key.'は全角カタカナで入力して下さい。';
-            $this->result = false;
+            $this->result = FALSE;
         }
         return $error; 
     }
@@ -78,92 +90,154 @@ class CommonValidator {
     //---------------------------------------
 
     public function mailValidation($key, $value) {
-        $error = false;
+        $error = FALSE;
         if(empty($value)) {
             $error = $key.'は必須入力です。';
-            $this->result = false;
+            $this->result = FALSE;
         }elseif(!preg_match('/^([a-zA-Z0-9])+([a-zA-Z0-9\._-])*@([a-zA-Z0-9_-])+([a-zA-Z0-9\._-]+)+$/',$value)) {
             $error = $key.'を正しく入力して下さい。';
-            $this->result = false;
+            $this->result = FALSE;
         }
         return $error;  
     }
 
+    /**
+     * メールアドレス重複確認
+     * $mailをキーにカスタマー情報を取得できた場合は今回のメールと比較し他ユーザとの重複か確認
+     * @param string $mail　ユーザーのメールアドレス
+     * @return CustomerDto | boolean FALSE
+     * @throws MyPDOException 
+     */
     public function checkMail($mail, $customerMail){
-        $error = false;
-        $customerDao = new \Models\CustomerDao();
-        $customerDto = $customerDao->checkMailExists($mail);
-        if($customerDto && $customerDto->getMail() != $customerMail){
-            $error =  "既に使用されているメールアドレスです。";
-            $this->result = false;     
+        
+        $error = FALSE;
+        $customerDao = new CustomerDao();
+        
+        try{
+            $customerDto = $customerDao->checkMailExists($mail);
+            
+            /*- カスタマー情報が取得でき、かつユーザ自身のアドレスでなかった場合はエラーとする。 -*/
+            if($customerDto && $customerDto->getMail() != $customerMail){
+                $error =  "既に使用されているメールアドレスです。";
+                $this->result = FALSE;     
+            }
+            return $error;
+        }catch(MyPDOException $e){
+            throw $e;
         }
-        return $error;
     }
     //---------------------------------------
     public function telValidation($key, $value) {
-        $error = false;
+        $error = FALSE;
         if(empty($value)) {
             $error = $key.'は必須入力です。';
-            $this->result = false;
+            $this->result = FALSE;
         }elseif(!preg_match('/^(0{1}\d{9,10})$/',$value)) {
             $error = $key.'は半角数字で市外局番から正しく入力してください。';
-            $this->result = false;
+            $this->result = FALSE;
         }
         return $error; 
     }
     
     //---------------------------------------
     public function firstZipCodeValidation($key, $value) {
-        $error = false;
+        $error = FALSE;
         if(empty($value)) {
             $error = $key.'は必須入力です。';
-            $this->result = false;
+            $this->result = FALSE;
         }elseif(!preg_match('/^\d{3}$/',$value)) {
-            $errors = $key.'を正しく入力して下さい。';
-            $this->result = false;
+            $error = $key.'を正しく入力して下さい。';
+            $this->result = FALSE;
         }
         return $error; 
     }
+    
     //---------------------------------------
     public function lastZipCodeValidation($key, $value) {
-        $error = false;
+        $error = FALSE;
         if(empty($value)) {
             $error = $key.'は必須入力です。';
-            $this->result = false;
+            $this->result = FALSE;
         }elseif(!preg_match('/^\d{4}$/',$value)) {
-            $errors = $key.'を正しく入力して下さい。';
-            $this->result = false;
+            $error = $key.'を正しく入力して下さい。';
+            $this->result = FALSE;
         }
         return $error; 
     }
+    
+    //---------------------------------------
+    /**
+     * 都道府県の値確認
+     * 用意された値でなければ例外を投げる
+     * 返り値なし
+     * @throws InvalidParamException
+     */
+    public function checkPrefecture($prefecture){
 
+        $prefectureCheck = FALSE;
+        foreach(Config::PREFECTURES as $kenmei){
+            if($prefecture == $kenmei){
+                $prefectureCheck = TRUE;   
+            }
+        }
+        if(!$prefectureCheck){
+            throw new InvalidParamException('Invalid param in prefecture:$prefecture='.$prefecture);
+        }
+    }
+    
     //---------------------------------------
       //パスワードチェック
     public function passValidation($key, $value) {
-        $error = false;
+        $error = FALSE;
         if(empty($value)) {
             $error = $key.'は必須入力です。';
-            $this->result = false;
+            $this->result = FALSE;
         }elseif(!preg_match("/^(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,20}$/", $value)){
             $error = $key.'は英字・数字を含め8～20文字で入力してください。';
-            $this->result = false;
+            $this->result = FALSE;
         }
         return $error; 
     }
     //--------------------------------------- 
     //パスワード(再確認)チェック
     public function passConfirmValidation($key, $value, $confirm) {
-        $error = false;
+        $error = FALSE;
         if(empty($value)) {
             $error = $key.'は必須入力です。';
-            $this->result = false;
+            $this->result = FALSE;
         }elseif($value != $confirm){
             $error = $key.'が一致しません。';
-            $this->result = false;
+            $this->result = FALSE;
         }
         return $error; 
     }
     
+    //---------------------------------------
+    public function priceValidation($key, $value) {
+        $error = FALSE;
+        if(empty($value)) {
+            $error = $key.'は必須入力です。';
+            $this->result = FALSE;
+        }elseif(!preg_match('/^(0|[1-9]\d*)$/',$value)) {
+            $error = $key.'を正しく入力して下さい。';
+            $this->result = FALSE;
+        }
+        return $error; 
+    }
+    
+    //---------------------------------------
+    public function numberValidation($key, $value) {
+        $error = FALSE;
+        if(!$value==0 && empty($value)) {
+            $error = $key.'は必須入力です。';
+            $this->result = FALSE;
+        }elseif(!preg_match('/^\d{0,7}$/',$value)) {
+            $error = $key.'を正しく入力して下さい。';
+            $this->result = FALSE;
+        }
+        return $error; 
+    }
+
     public function getResult(){
         return $this->result;
     }
