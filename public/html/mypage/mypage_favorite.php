8d403a2adadbfaad70c48db7b50a2e775ad77709
kyokokitazawa <59463972+KyokoKitazawa0322@users.noreply.github.com>
2020-07-04 23:06:03 +0900

    商品ステータス確認及び表示機能追加、出力時エスケープ処理追加等

--- public/html/mypage/mypage_favorite.php
+++ public/html/mypage/mypage_favorite.php
@@ -4,6 +4,8 @@ require_once (__DIR__ ."/../../../vendor/autoload.php");
 session_cache_limiter('none');
 session_start();
 
+use \Config\Config;
+
 $favorite = new \Controllers\MyPageFavoriteAction();
 $favorite->execute();
 $favoriteItems = $favorite->getFavoriteDto();
@@ -55,34 +57,44 @@ $(function() {
                             <?php foreach($favoriteItems as $item): ?>
                                 <li class="products">
                                     <div class="product_inner">
-                                        <a class="product_link" href="/html/item_detail.php?item_code=<?php echo $item->getItemCode(); ?>">
-                                            <img src="/img/items/<?php echo $item->getItemImage();?>" alt="" />
+                                        <a class="product_link" <?php if($favorite->checkItemStatus($item->getItemStatus())):?>href="/html/item_detail.php?item_code=<?=Config::h($item->getItemCode());?>"<?php endif;?>>
+                                            <img src="<?=$item->getItemImagePath();?>" alt="" />
                                             <div class="item_txt_wrap">
-                                                <p class="item_name"><?php echo $item->getItemName();?></p>
-                                                <p class="item_list_price">&yen;<?php echo number_format($item->getItemPriceWithTax());?></p>
+                                                <p class="item_name"><?=Config::h($item->getItemName());?></p>
+                                                <p class="item_list_price">&yen;<?=Config::h(number_format($item->getItemPriceWithTax()));?></p>
                                             </div>
                                         </a>
                                     </div>
                                     <div class="fav_btn_area">
+                                        <?php if($item->getItemStatus()=="1"):?>
+                                            <div class="fav_btn_wrap">
+                                                <input type="button" class="btn_cmn_mid btn_design_02" value="カートにいれる" id="cartIn" data-value="<?=Config::h($item->getItemCode());?>">
+                                            </div>
+                                        <?php elseif($item->getItemStatus()=="2"):?>
+                                            <p class="status_text">この商品は現在、入荷待ちです。</p>
+                                        <?php elseif($item->getItemStatus()=="3"):?>
+                                            <p class="status_text">この商品は販売終了しました。</p>
+                                        <?php elseif($item->getItemStatus()=="4"):?>
+                                            <p class="status_text">この商品は現在、一時掲載を停止してます。</p>
+                                        <?php elseif($item->getItemStatus()=="5"):?>
+                                            <p class="status_text">この商品は現在、品切れ中です(入荷未定)。</p>
+                                        <?php endif;?>
                                         <div class="fav_btn_wrap">
-                                            <input type="button" class="btn_cmn_mid btn_design_02" value="カートにいれる" id="cartIn" data-value="<?php echo $item->getItemCode(); ?>">
-                                        </div>
-                                        <div class="fav_btn_wrap">
-                                            <input type="button" class="btn_cmn_01 btn_design_03" value="削除" id="delete" data-item="<?php echo $item->getItemCode();?>">
+                                            <input type="button" class="btn_cmn_01 btn_design_03" value="削除" id="delete" data-item="<?=Config::h($item->getItemCode());?>">
                                         </div>
                                     </div>
                                 </li>
-                            <?php endforeach; ?>
+                            <?php endforeach;?>
                         </ul>
                     <?php else:?>
                         <div class="txt_wrapper">
                             <p class="none_txt">お気に入りに登録されている商品はありません。</p>
                         </div>
-                    <?php endif; ?>
+                    <?php endif;?>
                 </div>
             </div>
         </div>
-     <?php require_once(__DIR__.'/mypage_common.php'); ?>
+     <?php require_once(__DIR__.'/mypage_common.php');?>
     </div>
     <div id="footer">
         <p class="copy">&copy; 2020 BUBUTA All Rights Reserved.</p>
