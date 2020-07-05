359670e0019fae9b75b333d9ea7683fa3daf3c26
kyokokitazawa <59463972+KyokoKitazawa0322@users.noreply.github.com>
2020-07-05 15:11:37 +0900

    ページ不正遷移対策処理追加、商品ステータス確認処理、及び購入ボタン表示非表示切り替え処理追加、エスケープ処理追加等

--- public/html/order/order_confirm.php
+++ public/html/order/order_confirm.php
@@ -4,6 +4,8 @@ require_once (__DIR__ ."/../../../vendor/autoload.php");
 session_cache_limiter('none');
 session_start();
 
+use \Config\Config;
+
 $orderConfirm = new \Controllers\OrderConfirmAction();
 $orderConfirm->execute();
 ?>
@@ -31,21 +33,24 @@ $orderConfirm->execute();
                 </div>
                 <div class="main_contents_inner">
                     <div class="cart_item_box"> 
-                        <?php if(isset($_SESSION['pay_type']) && $_SESSION['pay_type'] == "none"): ?>
-                            <h4 style="color:red;">決済方法が未選択です。</h4>  
+                        <?php if(isset($_SESSION['pay_type']) && $_SESSION['pay_type'] == "none"):?>
+                            <p class="purchase_error_text">決済方法が未選択です。</p>  
+                        <?php endif;?>
+                        <?php if(isset($_SESSION['purchase_error'])):?>
+                            <p class="purchase_error_text">購入できない商品が含まれてます。</p> 
                         <?php endif;?>
                         <h3 class="ttl_cmn">配送先住所</h3>
                         <div class="shipping_box_wrap">
                             <div class="shipping_box">
                                 <dl class="shipping_address">
                                     <dt>名前：</dt>
-                                    <dd><?= $_SESSION['delivery']['name'];?></dd>
+                                    <dd><?=Config::h($_SESSION['delivery']['name']);?></dd>
                                     <dt>郵便番号：</dt>
-                                    <dd><?= $_SESSION['delivery']['post'];?></dd>
+                                    <dd><?=Config::h($_SESSION['delivery']['post']);?></dd>
                                     <dt>電話番号：</dt>
-                                    <dd><?= $_SESSION['delivery']['tel'];?></dd>
+                                    <dd><?=Config::h($_SESSION['delivery']['tel']);?></dd>
                                     <dt>住所：</dt>
-                                    <dd><?= $_SESSION['delivery']['address'];?></dd>
+                                    <dd><?=Config::h($_SESSION['delivery']['address']);?></dd>
                                 </dl>
                             </div>
                             <div class="update_link_wrap">
@@ -81,29 +86,38 @@ $orderConfirm->execute();
                             $item_total_price = $cart['item_price_with_tax'] * $cart['item_quantity'];
                         ?>
                             <div class="cart_item">
+                                <?php if($cart["item_status"]=="2"):?>
+                                    <p class="status_text">この商品は現在、入荷待ちです。</p>
+                                <?php elseif($cart["item_status"]=="3"):?>
+                                    <p class="status_text">この商品は販売終了しました。</p>
+                                <?php elseif($cart["item_status"]=="4"):?>
+                                    <p class="status_text">この商品は現在、一時掲載を停止してます。</p>
+                                <?php elseif($cart["item_status"]=="5"):?>
+                                    <p class="status_text">この商品は現在、品切れ中です(入荷未定)。</p>
+                                <?php endif;?>
                                 <div class="cart_item_img">
-                                    <a href="">
-                                        <img src="/img/items/<?= $cart["item_image"]; ?>"/>
+                                    <a href="/html/item_detail.php?item_code=<?=Config::h($cart['item_code'])?>">
+                                        <img src="<?=Config::h($cart["item_image_path"]);?>"/>
                                     </a>
                                 </div>
                                 <div class="cart_item_txt">
-                                    <h4><?= $cart["item_name"]; ?></h4>
+                                    <h4><?=$cart["item_name"];?></h4>
                                     <dl class="buy_itemu_menu mod_order_info">
                                         <dt>価格:</dt>
                                         <dd>
-                                            &yen;<?= number_format($cart["item_price_with_tax"]); ?>(税込)
+                                            &yen;<?=Config::h(number_format($cart["item_price_with_tax"]));?>(税込)
                                         </dd>
                                     </dl>
                                     <dl class="buy_item_quantity mod_order_info">
                                         <dt>数量:</dt>
                                         <dd>
-                                            <?= $cart['item_quantity']; ?>個
+                                            <?=$cart['item_quantity'];?>個
                                         </dd>
                                     </dl>
                                     <dl class="mod_order_info mod_order_total">
                                         <dt>小計:</dt>
                                         <dd>
-                                            &yen;<?= number_format($item_total_price);?>(税込)
+                                            &yen;<?=Config::h(number_format($item_total_price));?>(税込)
                                         </dd>
                                     </dl>
                                 </div>
@@ -116,28 +130,35 @@ $orderConfirm->execute();
                             <div class="payment_details">
                                 <dl class="mod_payment mod_payment_details">
                                     <dt>商品点数</dt>
-                                    <dd><?= $_SESSION['order']['total_quantity']; ?>点</dd>
+                                    <dd><?=Config::h($_SESSION['order']['total_quantity']);?>点</dd>
                                     <dt>商品代金合計(税込)</dt>
-                                    <dd>&yen;<?= number_format($_SESSION['order']['total_amount']);?></dd>
+                                    <dd>&yen;<?=Config::h(number_format($_SESSION['order']['total_amount']));?></dd>
                                     <dt>送料</dt>
-                                    <dd>&yen;<?= $_SESSION['order']['postage'];?></dd>
+                                    <dd>&yen;<?=Config::h($_SESSION['order']['postage']);?></dd>
                                     <dt>内消費税</dt>
-                                    <dd>&yen;<?= number_format($_SESSION['order']['tax']);?></dd>
+                                    <dd>&yen;<?=Config::h(number_format($_SESSION['order']['tax']));?></dd>
                                 </dl>
                                 <div class="payment_total">
                                     <dl class="mod_payment mod_payment_total">
                                         <dt>ご注文合計</dt>
-                                        <dd>&yen;<?= number_format($_SESSION['order']['total_amount']+$_SESSION['order']['postage']);?></dd>
+                                        <dd>&yen;<?=Config::h(number_format($_SESSION['order']['total_amount']+$_SESSION['order']['postage']));?></dd>
                                     </dl>
                                 </div>
                             </div>
                         </div>
                     </div>
                      <div class="cart_button_area">
-                        <form action="order_complete.php" method="POST">
-                            <input type="submit" class="btn_cmn_l btn_design_01" value="注文を確定する"/>
-                            <input type="hidden" name="cmd" value="order_complete"/>
-                        </form>
+                        <?php if(!isset($_SESSION['purchase_error'])):?>
+                            <form action="/html/order/order_complete.php" method="POST">
+                                <input type="submit" class="btn_cmn_l btn_design_01" value="注文を確定する"/>
+                                <input type="hidden" name="token_order_complete" value="<?=$_SESSION['token']['order_complete'];?>">
+                            </form>
+                        <?php else:?>
+                            <form action="/html/cart.php" method="POST">
+                                <input type="submit" class="btn_cmn_l btn_design_01" value="カートに戻る"/>
+                                <input type="hidden" name="cmd" value="back_to_cart"/>
+                            </form>
+                        <?php endif;?>
                     </div>
                 </div>
             </div>
