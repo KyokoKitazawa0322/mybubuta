8c1b5500b37e5ddbe3ca29aecbd7b8d796164159
kyokokitazawa <59463972+KyokoKitazawa0322@users.noreply.github.com>
2020-07-07 13:48:09 +0900

    CSRFバリデーションクラス新設

---
+++ app/models/CsrfValidator.php
@@ -0,0 +1,62 @@
+<?php
+namespace Models;
+
+use \Models\InvalidParamException;
+
+class CsrfValidator {
+
+    const HASH_ALGO = 'sha256';
+
+    public static function generate(){
+        
+        if(session_status() === PHP_SESSION_NONE) {
+            session_start();
+        }
+        return hash(self::HASH_ALGO, session_id());
+    }
+
+    
+    /**
+    * throw InvalidParamException
+    **/
+    public static function validate($token){
+        
+        $result = self::generate() === $token;
+        
+        if(!$result) {
+            throw new InvalidParamException('CSRF validation failed.');
+        }
+    }
+
+    /**
+    * throw InvalidParamException
+    **/
+    public static function maketoken($formname){
+       
+        $token = sha1(uniqid(mt_rand(), true));
+        $_SESSION[$formname] = $token;
+        return $token;
+    }
+   
+    /**
+    * throw InvalidParamException
+    **/
+    public static function checkToken($token, $formName){
+        if(!$token){
+            throw new InvalidParamException('CSRF validation failed. -no token found');
+        }
+        
+        if(!isset($_SESSION[$formName])){
+            throw new InvalidParamException('CSRF validation failed. -no token found');
+        }
+        $key = $_SESSION[$formName];
+           
+        if($key !== $token ){
+            throw new InvalidParamException('CSRF validation failed. -invalid token');
+        }
+        unset($_SESSION[$formName]);   
+    }
+
+}
+
+?>
