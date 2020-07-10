d3b709d809ee782f50d6bce8b5e5a5ff545f458e
kyokokitazawa <59463972+KyokoKitazawa0322@users.noreply.github.com>
2020-07-10 17:09:21 +0900

    相対パス修正

--- public/html/mypage/notice/mypage_notice_detail.php
+++ public/html/mypage/notice/mypage_notice_detail.php
@@ -1,5 +1,5 @@
 <?php
-require_once (__DIR__ ."/../../../vendor/autoload.php");
+require_once (__DIR__ ."/../../../../vendor/autoload.php");
 
 session_cache_limiter('none');
 session_start();
@@ -24,7 +24,7 @@ $noticeDto = $mypageNoticeDetail->getNoticeDto();
 
 <body class="mypage" id="mypage_notice">
 <div class="wrapper">
-    <?php require_once(__DIR__.'/../common/header_common.php')?>
+    <?php require_once(__DIR__.'/../../common/header_common.php')?>
     <div class="container">
         <div class="main_wrapper">
             <div class="main_contents">
@@ -43,7 +43,7 @@ $noticeDto = $mypageNoticeDetail->getNoticeDto();
                 </div>
             </div>
         </div>
-        <?php require_once(__DIR__.'/mypage_common.php');?>
+        <?php require_once(__DIR__.'/../mypage_common.php');?>
         </div>
     <div id="footer">
         <p class="copy">&copy; 2020 BUBUTA All Rights Reserved.</p>
