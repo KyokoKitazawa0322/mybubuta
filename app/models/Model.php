fb614e786d991c0f0ae32f891e8aba96e5eac965
kyokokitazawa <59463972+KyokoKitazawa0322@users.noreply.github.com>
2020-07-10 16:29:23 +0900

    DB接続クラス修正(シングルトンクラスへ変更、PDOのtransaction/commit実行メソッド追加、例外処理修正等)

--- app/models/Model.php
+++ app/models/Model.php
@@ -2,33 +2,68 @@
 namespace Models;
 
 use \Config\Config;
+use \Models\DBConnectionException;
 
 class Model{
 
-    protected $pdo = NULL;
-
+    private static $instance = NULL;
+    private static $pdo = NULL;
+    
+    /**
+     * @throws DBConnectionException 
+     */
     public function __construct(){
-        
+
         $dbName = getenv('DB_NAME');
         $host = getenv('HOST');
         $dsn = "mysql:dbname=".$dbName.";charset=utf8;host=".$host;        
         $user = getenv('DB_USER');
         $password = getenv('PASSWORD');
 
-        if((!$this->pdo instanceof PDO)){
-            try{
-                $this->pdo = new \PDO($dsn, $user, $password, array(\PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION, \PDO::ATTR_PERSISTENT => true));
-                
-            }catch(\PDOException $e){
-                header('Content-Type: text/plain; charset=UTF-8', true, 500);
-                Config::outputLog($e->getCode(), '接続エラー:'.$e->getMessage(), $e->getTraceAsString());
-                exit('接続エラーが発生しました。しばらくたってから再度アクセスしてください。');
-            }
+        try{
+            self::$pdo = new \PDO($dsn, $db_user, $db_pass, array(\PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION, \PDO::ATTR_PERSISTENT => true));
+
+        }catch(\PDOException $e){
+            throw new DBConnectionException($e->getMessage(), (int)$e->getCode());
         }
     }
+    
+    public static function getInstance(){
+        if(is_null(self::$instance)){
+            self::$instance = new static();
+        }
+        return self::$instance;
+    }
 
-    public function __destruct()
-    {
-        $this->pdo = null;
+    public function getPdo(){
+        return self::$pdo;   
+    }
+    
+    private function __wakeup(){
+    }
+
+    private function __clone(){
+    }
+    
+    /**
+     * @throws DBConnectionException 
+     */
+    public function beginTransaction(){
+        try{
+            self::$pdo->beginTransaction();
+        }catch(\PDOException $e){
+            throw new MyPDOException($e->getMessage(), (int)$e->getCode());
+        }
+    }
+    
+    /**
+     * @throws DBConnectionException 
+     */
+    public function commit(){
+        try{
+            self::$pdo->commit();
+        }catch(\PDOException $e){
+            throw new MyPDOException($e->getMessage(), (int)$e->getCode());
+        }
     }
 }
