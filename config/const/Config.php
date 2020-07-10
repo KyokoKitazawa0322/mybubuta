7d4ea73a4faae55eafafee79030cbc64ddbb98c0
kyokokitazawa <59463972+KyokoKitazawa0322@users.noreply.github.com>
2020-07-10 16:34:46 +0900

    ログ出力メソッド削除

--- config/const/Config.php
+++ config/const/Config.php
@@ -17,15 +17,6 @@ class Config{
     
     const PREFECTURES = array('北海道', '青森県', '岩手県', '宮城県', '秋田県', '山形県', '福島県','茨城県','栃木県','群馬県', '埼玉県','千葉県', '東京都', '神奈川県','新潟県',' 富山県','石川県','福井県','山梨県','長野県','岐阜県','静岡県','愛知県','三重県','滋賀県','京都府','大阪府','兵庫県','奈良県','和歌山県','鳥取県','島根県','岡山県','広島県', '山口県','徳島県','香川県','愛媛県','高知県','福岡県','佐賀県','長崎県','熊本県','大分県','宮崎県','鹿児島県','沖縄県');
     
-    /**
-     * @param Exception $e
-     */
-    public static function outputLog(\Exception $e){
-        date_default_timezone_set('Asia/Tokyo');
-        $datetime = date( "Y/m/d H:i:s");
-        $message = "[time]=>{$datetime}\r\n[ExceptionCode]=>{$e->getCode()}\r\n[Message]=>{$e->getMessage()}\r\n[File/Line]=>{$e->getFile()}/{$e->getLine()}\r\n[Trace]=>{$e->getTraceAsString()}";
-        error_log($message);
-    }
 
     public static function h($s){
       return htmlspecialchars($s, ENT_QUOTES, 'UTF-8');
