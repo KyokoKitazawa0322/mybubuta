f171027b4a89e363dc916ea1cf5487a5c3b1fd5b
kyokokitazawa <59463972+KyokoKitazawa0322@users.noreply.github.com>
2020-07-23 13:21:01 +0900

    複数ウィンドウでの処理対応化のための修正、バリデーション修正、HTTPメソッド値受け取り処理修正(Configクラスメソッド使用)等

--- app/controllers/AdminNoticeRegisterAction.php
+++ app/controllers/AdminNoticeRegisterAction.php
@@ -4,9 +4,11 @@ use \Models\NoticeDao;
 use \Models\NoticeDto;
 use \Models\Model;
 
+use \Models\CommonValidator;
 use \Models\CsrfValidator;
 use \Config\Config;
 
+use \Models\InvalidParamException;
 use \Models\DBParamException;
 use \Models\NoRecordException;
 use \Models\MyPDOException;
@@ -16,12 +18,18 @@ class AdminNoticeRegisterAction{
     
     private $item;
     
+    private $titleError;
+    private $mainTextError;
+    
+    private $sessionKey;
+    private $errorMessage = "none";
+    
     public function execute(){
         
         /*====================================================================
       　  $_SESSION['admin_id']がなければadmin_login.phpへリダイレクト
         =====================================================================*/
-        $cmd = filter_input(INPUT_POST, 'cmd');
+        $cmd = Config::getPOST("cmd");
         
         if($cmd == "admin_logout"){
             unset($_SESSION['admin_id']);    
@@ -32,35 +40,85 @@ class AdminNoticeRegisterAction{
             exit();
         }
         
+        $sessionKey = "admin_notice_register-".rand(5,10);
+        $this->sessionKey = $sessionKey;
+        
+        $password = Config::getPOST("password");
+        
         if($cmd == "admin_notice_register"){
             
-            $token = filter_input(INPUT_POST, "token");
+            $token = Config::getPOST( "token");
             try{
                 CsrfValidator::validate($token);
             }catch(InvalidParamException $e){
                 $e->handler($e);   
             }
             
-            $title = filter_input(INPUT_POST, 'title', FILTER_SANITIZE_STRING); 
-            $mainText = filter_input(INPUT_POST, 'main_text', FILTER_SANITIZE_STRING); 
+            $title = Config::getPOST('title'); 
+            $mainText = Config::getPOST('main_text'); 
+            
+            $_SESSION[$sessionKey] = array (
+                'title' => $title,    
+                'mainText' => $mainText
+            );
+            
+            $validator = new CommonValidator();
+            
+            $key = "件名";
+            $limit = 50;
+            $this->titleError = $validator->textAreaValidation($key, $title, $limit); 
+            
+            $key = "本文";
+            $limit = 1000;
+            $this->mainTextError = $validator->textAreaValidation($key, $mainText, $limit); 
                 
-            try{
-                $model = Model::getInstance();
-                $pdo = $model->getPdo();
-                $noticeDao = new NoticeDao($pdo);
-                $noticeDto = $noticeDao->insertNoticeInfo($title, $mainText);
+            if($validator->getResult()){
+                $confirmPassword = getenv('CONFIRM_PASSWORD');
+                
+                if($password !== $confirmPassword){
+                    try{
+                        $model = Model::getInstance();
+                        $pdo = $model->getPdo();
+                        $noticeDao = new NoticeDao($pdo);
+                        $noticeDto = $noticeDao->insertNoticeInfo($title, $mainText);
+                        
+                        unset($_SESSION[$sessionKey]);
 
-            }catch(DBConnectionException $e){
-                $e->handler($e);   
-                
-            } catch(MyPDOException $e){
-                $e->handler($e);
+                    }catch(DBConnectionException $e){
+                        $e->handler($e);   
+
+                    } catch(MyPDOException $e){
+                        $e->handler($e);
+                    }
+                }else{
+                    $this->errorMessage = "デモ画面のため、実際の登録はできません。";
+                }
             }
         }
     }
     
+    /*---------------------------------------*/
     public function getItem(){
         return $this->noticeDto;   
+    }
+    
+    public function getTitleError(){
+        return $this->titleError;   
+    }
+    
+    public function getMailTextError(){
+        return $this->mainTextError;   
+    }
+    
+    public function getErrorMessage(){
+        return $this->errorMessage;   
+    }
+
+    public function echoValue($value){
+        $sessionKey = $this->sessionKey;
+        if(isset($_SESSION[$sessionKey][$value])){
+            echo $_SESSION[$sessionKey][$value];
+        }
     }
 }
 ?>
