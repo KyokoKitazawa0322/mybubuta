c15c5df85b9ca4c9c389dd5a1c889707e0ce6e18
kyokokitazawa <59463972+KyokoKitazawa0322@users.noreply.github.com>
2020-07-23 12:30:07 +0900

    例外発生時エラーメッセージ変更

--- app/models/DBParamException.php
+++ app/models/DBParamException.php
@@ -6,7 +6,7 @@ class DBParamException extends \Models\OriginalException{
     public function __construct($message){
         
         parent::__construct($message, 400);
-        $this->setUserMessage("不正な値が検出されました");
+        $this->setUserMessage("不正な操作です。");
     }
 }
 ?>
