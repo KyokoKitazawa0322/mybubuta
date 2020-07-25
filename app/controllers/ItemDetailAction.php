9f7b385622972fc384320e43bee284381341d171
kyokokitazawa <59463972+KyokoKitazawa0322@users.noreply.github.com>
2020-07-25 21:40:29 +0900

    在庫表示機能追加(在庫が10以下の時)、購入数量確認処理追加、SESSION変数の効率化、HTTPメソッド値受け取り処理修正(Configクラスメソッド使用)等

--- app/controllers/ItemDetailAction.php
+++ app/controllers/ItemDetailAction.php
@@ -10,16 +10,18 @@ use \Config\Config;
 use \Models\DBParamException;
 use \Models\NoRecordException;
 use \Models\MyPDOException;
+use \Models\InvalidParamException;
 use \Models\DBConnectionException;
 
 class ItemDetailAction {
     
     private $item;
+    private $itemStock;
     
     public function execute() {
         
-        $itemCode = filter_input(INPUT_GET, 'item_code');
-        $_SESSION['add_cart'] = 'undone';
+        $cmd = Config::getPOST('cmd');
+        $itemCode = Config::getGET('item_code');
         
         try{
             $model = Model::getInstance();
@@ -27,6 +29,7 @@ class ItemDetailAction {
             $itemsdao = new ItemsDao($pdo);   
             $item = $itemsdao->getItemByItemCodeForDetail($itemCode);
             $this->item = $item;
+            $this->itemStock = $item->getItemStock();
             
         }catch(DBConnectionException $e){
             $e->handler($e);   
@@ -37,10 +40,43 @@ class ItemDetailAction {
         }catch(DBParamException $e){
             $e->handler($e);
         }
+    
+        if($cmd == "add_cart"){
+            $itemQuantity = Config::getPOST('item_quantity');
+            
+            try{
+                $this->checkItemQuantity($itemQuantity);
+                
+                $_SESSION['add_cart'] = array(
+                    "item_code" => $itemCode,
+                    "item_quantity" => $itemQuantity
+                );      
+            } catch(InvalidParamException $e){
+                $e->handler($e);
+            }
+            header('Location:/html/cart.php');
+            exit();
+        }
     }
     
     public function getItem(){
         return $this->item;   
+    }
+    
+    public function alertStock(){
+        $itemStock = $this->itemStock;
+        if($itemStock <= 10){
+            return true;   
+        }else{
+            return false;
+        }   
+    }
+    
+    public function checkItemQuantity($itemQuantity){
+        $itemStock = $this->itemStock;
+        if($itemQuantity>$itemStock){
+            throw new InvalidParamException('Invalid param for add_cart: $_POST["item_quantity"]='.$itemQuantity." > item_stock=".$itemStock);   
+        } 
     }
 }
     
