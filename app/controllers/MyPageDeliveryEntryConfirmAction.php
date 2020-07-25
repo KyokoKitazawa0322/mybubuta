900c16c24a3e87d2099609160ef80ab58a4643e8
kyokokitazawa <59463972+KyokoKitazawa0322@users.noreply.github.com>
2020-07-25 22:32:57 +0900

    複数タブ・ウィンドウでの同時処理対応化のための修正、値検証及び表示メソッド追加、HTTPメソッド値受け取り処理修正(Configクラスメソッド使用)等

--- app/controllers/MyPageDeliveryEntryConfirmAction.php
+++ app/controllers/MyPageDeliveryEntryConfirmAction.php
@@ -5,13 +5,21 @@ use \Models\InvalidParamException;
 
 class MyPageDeliveryEntryConfirmAction extends \Controllers\CommonMyPageAction{
     
+    private $keyForUpdate;
+    private $delId;
+    
     public function execute(){
         
-        $cmd = filter_input(INPUT_GET, 'cmd');
+        $postCmd = Config::getPOST('cmd');
         
-        $this->checkLogoutRequest($cmd);
+        $this->checkLogoutRequest($postCmd);
         $this->checkLogin();
         $customerId = $_SESSION['customer_id'];
+        
+        $delId = Config::getGET('del_id');       
+        $this->delId = $delId;   
+        $keyForUpdate = "del_update-".$delId;
+        $this->keyForUpdate = $keyForUpdate;
         
         try{
             $this->checkValidationResult();
@@ -29,14 +37,27 @@ class MyPageDeliveryEntryConfirmAction extends \Controllers\CommonMyPageAction{
     * throw InvalidParamException
     **/
     public function checkValidationResult(){
-        
-        if(!isset($_SESSION['delivery_entry_data']) || $_SESSION['delivery_entry_data'] != "complete"){
-            if(!isset($_SESSION['delivery_entry_data'])){
+        $keyForUpdate = $this->keyForUpdate;
+        if(!isset($_SESSION[$keyForUpdate]['delivery_entry_data']) || $_SESSION[$keyForUpdate]['delivery_entry_data'] != "complete"){
+            if(!isset($_SESSION[$keyForUpdate]['delivery_entry_data'])){
                 $deliveryEntryData = "nothing";
             }else{
-                $deliveryEntryData = $_SESSION['delivery_entry_data'];   
+                $deliveryEntryData = $_SESSION[$keyForUpdate]['delivery_entry_data'];   
             }
-            throw new InvalidParamException('Invalid param for update_confirm:$_SESSION["delivery_entry_data"]='.$deliveryEntryData);
+            throw new InvalidParamException('Invalid param for update_confirm:$_SESSION[$keyForUpdate]["delivery_entry_data"]='.$deliveryEntryData);
+        }
+    }
+    
+    public function getDelId(){
+        return $this->delId;   
+    }
+    
+    public function echoValue($value){
+        $keyForUpdate = $this->keyForUpdate;
+        if(isset($_SESSION[$keyForUpdate][$value])){
+            echo $_SESSION[$keyForUpdate][$value];
+        }else{
+            echo $customerDate;
         }
     }
 }
