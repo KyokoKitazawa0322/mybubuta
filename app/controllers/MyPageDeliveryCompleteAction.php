e6fb9ea9ec92084b4841d5120ef84800af21754d
kyokokitazawa <59463972+KyokoKitazawa0322@users.noreply.github.com>
2020-07-25 23:06:22 +0900

    複数タブ・ウィンドウでの同時処理対応化のための修正、HTTPメソッド値受け取り処理修正(Configクラスメソッド使用)等

--- app/controllers/MyPageDeliveryCompleteAction.php
+++ app/controllers/MyPageDeliveryCompleteAction.php
@@ -3,6 +3,7 @@ namespace Controllers;
 
 use \Models\DeliveryDao;
 use \Models\CustomerDao;    
+use \Models\Model;
 
 use \Config\Config;
 use \Models\CsrfValidator;
@@ -11,12 +12,15 @@ use \Models\DBParamException;
 use \Models\NoRecordException;
 use \Models\InvalidParamException;
 use \Models\MyPDOException;
+use \Models\DBConnectionException;
 
 class MyPageDeliveryCompleteAction extends \Controllers\CommonMyPageAction{
     
+    private $keyForUpdate;
+        
     public function execute(){
         
-        $cmd = filter_input(INPUT_POST, 'cmd');
+        $cmd = Config::getPOST("cmd");
         
         $this->checkLogoutRequest($cmd);
         $this->checkLogin();
@@ -26,7 +30,7 @@ class MyPageDeliveryCompleteAction extends \Controllers\CommonMyPageAction{
          mypage_delivery_entry_confirm.phpで「この内容で登録をする」ボタンが押された時の処理
          =====================================================================*/
         
-        $token = filter_input(INPUT_POST, "token_del_entry_complete");
+        $token = Config::getPOST( "token_del_entry_complete");
         $formName = "token_del_entry_complete";
         
         try{
@@ -35,29 +39,39 @@ class MyPageDeliveryCompleteAction extends \Controllers\CommonMyPageAction{
             $e->handler($e);   
         }
 
-        $lastName = $_SESSION['del_update']['last_name'];
-        $firstName = $_SESSION['del_update']['first_name'];
-        $rubyLastName = $_SESSION['del_update']['ruby_last_name'];
-        $rubyFirstName = $_SESSION['del_update']['ruby_first_name'];
-        $zipCode01 = $_SESSION['del_update']['zip_code_01'];
-        $zipCode02 = $_SESSION['del_update']['zip_code_02'];
-        $prefecture = $_SESSION['del_update']['prefecture'];
-        $city = $_SESSION['del_update']['city'];
-        $blockNumber = $_SESSION['del_update']['block_number'];
-        $buildingName = $_SESSION['del_update']['building_name'];
-        $tel = $_SESSION['del_update']['tel'];
-
-        $deliveryId = $_SESSION['del_id'];
+        $delId = Config::getGET('del_id');
+        $keyForUpdate = "del_update-".$delId;
+        
+        try{    
+            if(isset($_SESSION[$keyForUpdate])){
+                $this->keyForUpdate = $keyForUpdate;
+            }else{
+                throw new InvalidParamException('Invalid param for $keyForUpdate='.$keyForUpdate);
+            }
+        }catch(InvalidParamException $e){
+            $e->handler($e);   
+        }
+        
+        
+        $lastName = $_SESSION[$keyForUpdate]['last_name'];
+        $firstName = $_SESSION[$keyForUpdate]['first_name'];
+        $rubyLastName = $_SESSION[$keyForUpdate]['ruby_last_name'];
+        $rubyFirstName = $_SESSION[$keyForUpdate]['ruby_first_name'];
+        $zipCode01 = $_SESSION[$keyForUpdate]['zip_code_01'];
+        $zipCode02 = $_SESSION[$keyForUpdate]['zip_code_02'];
+        $prefecture = $_SESSION[$keyForUpdate]['prefecture'];
+        $city = $_SESSION[$keyForUpdate]['city'];
+        $blockNumber = $_SESSION[$keyForUpdate]['block_number'];
+        $buildingName = $_SESSION[$keyForUpdate]['building_name'];
+        $tel = $_SESSION[$keyForUpdate]['tel'];
 
         try{
             $model = Model::getInstance();
             $pdo = $model->getPdo();
             $deliveryDao = new DeliveryDao($pdo);
-            $deliveryDao->updateDeliveryInfo($lastName, $firstName, $rubyLastName, $rubyFirstName, $zipCode01, $zipCode02, $prefecture, $city, $blockNumber, $buildingName, $tel, $customerId, $deliveryId);
+            $deliveryDao->updateDeliveryInfo($lastName, $firstName, $rubyLastName, $rubyFirstName, $zipCode01, $zipCode02, $prefecture, $city, $blockNumber, $buildingName, $tel, $customerId, $delId);
 
-            unset($_SESSION['delivery_entry_data']);
-            unset($_SESSION['del_update']);
-            unset($_SESSION['del_id']);
+            unset($_SESSION[$keyForUpdate]);
 
         }catch(DBConnectionException $e){
             $e->handler($e);  
@@ -73,8 +87,8 @@ class MyPageDeliveryCompleteAction extends \Controllers\CommonMyPageAction{
             order_delivery_listからきた場合
         ————————————————————————————————————————————————————————————————*/
 
-        if(isset($_SESSION['from_order_flag'])){
-            unset($_SESSION['from_order_flag']);
+        if(isset($_SESSION['track_for_order']) && $_SESSION['track_for_order']=="order_delivery_list"){
+            unset($_SESSION['track_for_order']);
             header('Location:/html/order/order_delivery_list.php');
             exit();
         }
